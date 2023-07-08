ebf319c3fad2865c04f65231f5e8f8d53a6dda0d
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-08 16:53:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,8 +170,6 @@ def cria_mapa(regiao):
 
     if região == 'Nordeste':
         carga_estados['cores']=[200,None,None,None]
-        mapa = folium.Map(location=[-14.235,-54.2],zoom_start=4,
-                    max_zoom=4,min_zoom=4,tiles='CartoDB positron',dragging=False)
 
         cloropleth = folium.Choropleth(
         geo_data=coleta_localizacao(),
@@ -182,8 +180,6 @@ def cria_mapa(regiao):
         )
         cloropleth.geojson.add_to(mapa)
         carga_estados.set_index('Estados',inplace=True)
-    
-    
     
         for features in cloropleth.geojson.data['features']:
           features['properties']['MHW'] = "Carga diária" + " : " + str(carga_estados.loc[features['properties']['NOME2']]['Mhw'])
