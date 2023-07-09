d8118ed25fc1a10452670b7326c5efdf0abdadae
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-09 13:21:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,7 +194,7 @@ def cria_mapa_nordeste():
         )
     hash(st_mapa = st_folium(mapa, width=1000, height=450,key='Nordeste'))
     if st_mapa['last_active_drawing']:
-      st.write('ola')
+      st.write('kkk')
       return st_mapa['last_active_drawing']['properties']['NOME2']
 
 
