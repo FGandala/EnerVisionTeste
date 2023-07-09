e36397d27638e519066e5dfbaa4361cc54ff04cc
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-09 15:02:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,7 +117,7 @@ def cria_grafico_linhas(dados_centro_sul):
     }
 ], 'overlaid')
 
-@st.cache_data(experimental_allow_widgets=True)
+#@st.cache_data(experimental_allow_widgets=True)
 def cria_mapa(_cores):
     DATA=('https://ons-dl-prod-opendata.s3.amazonaws.com/dataset/carga_energia_di/CARGA_ENERGIA_2023.csv')
     carga=pd.read_csv(DATA,delimiter=';')
