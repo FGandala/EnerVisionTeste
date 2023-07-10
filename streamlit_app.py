bcc0609fd882c1a061418063749116f33227d98a
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-10 20:23:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ def filtra_dados(região,data_frame):
   return dados
 
 def cria_grafico_linhas(dados,região,tempo_inicial,tempo_final):
-  grafico=alt.Chart(dados).mark_area(color = 'lightyellow',
-                           opacity = 0.5,
-                           line = {'color':'lightyellow'}).encode(
+  grafico=alt.Chart(dados).mark_area(color = 'orange',
+                           opacity = 1,
+                           line = {'color':'orange'}).encode(
      
   
     alt.X('Datetime'),
