ea6b63004646a62b39d96f4c833b42514a9748c6
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-11 17:12:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,12 +102,13 @@ def home():
                                               max_value=fim,
                                               )
     
+    opção_tempo_inicial=datetime(opção_tempo_incial.year,opção_tempo_incial.month,opção_tempo_inicial.day,0,0,0)
   
     opção_tempo_final = st.sidebar.date_input('Escolha uma data final',opção_tempo_inicial,min_value=opção_tempo_inicial,
                                               max_value=fim,
                                               )
 
-
+    opção_tempo_final=datetime(opção_tempo_final.year,opção_tempo_final.month,opção_tempo_final.day,23,0,0)
     
     st.altair_chart(cria_grafico_linhas(filtra_dados(opção_regiao,opção_tempo_inicial,opção_tempo_final),opção_regiao), theme="streamlit", use_container_width=True)
 
