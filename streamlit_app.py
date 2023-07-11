541fd23cee7dec122d23554757127832c00f476f
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-11 17:49:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def coleta_localizacao():
   return localizacao
 def filtra_dados(região,tempo_inicial,tempo_final):
   tempo_inicial=datetime.datetime(tempo_inicial.year,tempo_inicial.month,tempo_inicial.day,0,0,0)
-  tempo_final=datetime.datetime(tempo_final.year,tempo_final.month,tempo_final.day,0,0,0)
+  tempo_final=datetime.datetime(tempo_final.year,tempo_final.month,tempo_final.day,23,0,0)
   data_frame=coleta_dados_csv()
   dados = data_frame[[região,'Datetime']]
   dados['Datetime']=pd.to_datetime(dados['Datetime'])
