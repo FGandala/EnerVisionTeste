9d2d241254a9d0eea0a7c8212a2c82f931805fcb
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-11 18:08:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def filtra_dados(região,tempo_inicial,tempo_final):
     a=3
   else:
     filtrados=dados.loc[(dados['Datetime']>=tempo_inicial)&(dados['Datetime']<=tempo_final)]
-    filtrados['Datetime']=filtrados['Datetime'].dt.time.normalize()
+    filtrados['Datetime']=filtrados['Datetime'].dt.time
     st.write(filtrados)
     return filtrados
 def cria_grafico_linhas(dados,região):
