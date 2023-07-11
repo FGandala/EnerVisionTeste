c71896d6bdfdf62ff40f0480dd8801214d08fb7c
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-11 17:18:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def coleta_localizacao():
   localizacao = gpd.read_file('grandes_regioes_json.geojson')
   return localizacao
 def filtra_dados(região,tempo_inicial,tempo_final):
-  tempo_inicial=datetime(tempo_incial.year,tempo_incial.month,tempo_inicial.day,0,0,0)
+  tempo_inicial=datetime(tempo_inicial.year,tempo_inicial.month,tempo_inicial.day,0,0,0)
   tempo_final=datetime(tempo_final.year,tempo_final.month,tempo_final.day,23,0,0)
   data_frame=coleta_dados_csv()
   dados = data_frame[[região,'Datetime']]
