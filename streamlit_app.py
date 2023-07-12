555b3d7f56df7fae60587b65637ba13e4f187309
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-12 20:32:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def filtra_dados(região,tempo_inicial,tempo_final):
     filtrados.set_index('Datetime',inplace=True)
     filtrados= filtrados.resample('D').sum()
     filtrados = filtrados.reset_index()
-    filtrados['Datetime']=filtrados['Datetime'].dt.strftime("%m/%d")
+    filtrados['Datetime']=filtrados['Datetime'].dt.strftime("%d/%m")
     filtrados.rename(columns={região:'Mhw','Datetime':'Tempo'},inplace=True)
     return filtrados
   
