938c7861ae35a6420f947efa5a209442b7f85154
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-12 13:11:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def cria_grafico_linhas(dados):
     opacity=alt.condition(pontos_proximos, alt.value(1), alt.value(0))
       )
   texto = grafico.mark_text(align='center', dx=0, dy=-30,color='orange',size=20).encode(
-    text=alt.condition(pontos_proximos, 'label:Mhw', alt.value(' '))
+    text=alt.condition(pontos_proximos, 'Mhw:N', alt.value(' '))
     
       ).transform_calculate(label='datum.y + " Mhw"')
   regua = alt.Chart(dados).mark_rule(color='gray').encode(
