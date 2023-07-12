bd35867bfb78fa90609f03b5b06eecb4a9112302
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-11 22:11:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ def filtra_dados(região,tempo_inicial,tempo_final):
   tempo_final=datetime.datetime(tempo_final.year,tempo_final.month,tempo_final.day,23,0,0)
   data_frame=coleta_dados_csv()[[região,'Datetime']]
   data_frame['Datetime']=pd.to_datetime(data_frame['Datetime'])
-  
+  if tempo_inicial.year != tempo_final.year:
+    a=1
   elif tempo_inicial.month != tempo_final.month:
     a=2
   elif tempo_inicial.day != tempo_final.day: 
