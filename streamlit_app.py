3e086163ae9cf699f155f962b94bd622513162b0
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-12 13:26:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def cria_grafico_linhas(dados):
   texto = grafico.mark_text(align='center', dx=0, dy=-30,color='orange',size=20).encode(
     text=alt.condition(pontos_proximos, 'Mhw:N', alt.value(' '))
     
-      ).transform_calculate(label=label='"INV: " + datum.Mhw')
+      ).transform_calculate(label='"INV: " + datum.Mhw')
   regua = alt.Chart(dados).mark_rule(color='gray').encode(
     x='Tempo',y='Mhw',
   ).transform_filter(
