85ae686bb452c0855ab2acf7886ccdc325ba98f7
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-15 16:48:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def filtra_dados(região,tempo_inicial,tempo_final):
     filtrados = filtrados.reset_index()
     meses_numeros = filtrados['Datetime'].dt.month
     meses = meses_numeros.apply(lambda x: numero_mes[x])
-    filtrados['Datetime']=meses['Datetime'].to_string()
+    filtrados['Datetime']=meses['Datetime'].astype(str)
     filtrados.rename(columns={região:'Mhw','Datetime':'Tempo'},inplace=True)
     st.write(filtrados)
     st.write(filtrados.dtypes)
