8754fb0e281e80846e56ecec0dcc0e27b3ee549b
Francisco Gandala <96669523+FGandala@users.noreply.github.com>
2023-07-17 10:33:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,7 @@ def home():
     
     st.altair_chart(cria_grafico_linhas(filtra_dados(opção_regiao,opção_tempo_inicial,opção_tempo_final)), theme="streamlit", use_container_width=True)
     teste = st_js.st_javascript("window.innerWidth")
-
+    st.write(teste)
 
 
 
